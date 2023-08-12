4ef2711ee5e052b62a91f128e8fe15663677548b
luisguilheerme <lg.maroni@gmail.com>
2023-08-12 09:16:42 -0300

    User and Role

--- src/main/java/com/luisguilherme/dslearn/DsLearnApplication.java
+++ src/main/java/com/luisguilherme/dslearn/DsLearnApplication.java
@@ -1,4 +1,4 @@
-package com.luisguilherme.dspost;
+package com.luisguilherme.dslearn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
