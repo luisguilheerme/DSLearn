98cad3b6423ecf452d94892672a9b962953e6a8f
luisguilheerme <lg.maroni@gmail.com>
2023-08-14 19:21:39 -0300

    Deliver, database seed

--- src/main/java/com/luisguilherme/dslearn/entities/Enrollment.java
+++ src/main/java/com/luisguilherme/dslearn/entities/Enrollment.java
@@ -1,7 +1,9 @@
 package com.luisguilherme.dslearn.entities;
 
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import com.luisguilherme.dslearn.entities.pk.EnrollmentPK;
@@ -10,6 +12,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -28,6 +31,9 @@ public class Enrollment {
 	
 	@ManyToMany(mappedBy = "enrollmentsDone")
 	private Set<Lesson> lessonsDone = new HashSet<>();
+	
+	@OneToMany(mappedBy = "enrollment")
+	private List<Deliver> deliveries = new ArrayList<>();
 	
 	public Enrollment() {
 		
